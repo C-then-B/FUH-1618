9b70cbba0e01b01f77e82065cbd12f39757e7492
SLVR <cdevroot@gmail.com>
2022-04-15 18:07:33 +0700

    add EA 2.2

---
+++ EA 2/EA 2.2/Stack.java
@@ -0,0 +1,55 @@
+public class Stack {
+    class Entry {
+        String value;
+        Entry next;
+
+        Entry(String value, Entry next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Entry head = new Entry("", null);
+    private int size;
+
+    public Stack() {
+        head.next = head;
+    }
+
+    void push(String s) {
+        Entry newEntry = new Entry(s, head);
+        head = newEntry;
+        size++;
+    }
+
+    public String peek() {
+        checkValidity();
+
+        return head.value;
+    }
+
+    public String pop() {
+        checkValidity();
+
+        Entry popped = head;
+        head = popped.next;
+        size--;
+        return popped.value;
+    }
+
+    private void checkValidity() {
+        if (size == 0)
+            throw new java.util.NoSuchElementException();
+    }
+
+    public String toString() {
+        String out = "[";
+        Entry curr = head;
+        for (int i = 0; i < size; i++) {
+            out = i + 1 == size ? out + curr.value : out + curr.value + ", ";
+            curr = curr.next;
+        }
+
+        return out + "]";
+    }
+}
---
+++ EA 2/EA 2.2/StackTest.java
@@ -0,0 +1,44 @@
+public class StackTest {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        try {
+            stack.peek();
+        } catch (Exception e) {
+            System.out.println("obj.peek() exception caught: " + e);
+        }
+        try {
+            stack.pop();
+        } catch (Exception e) {
+            System.out.println("obj.pop() exception caught: " + e);
+        }
+        System.out.println();
+
+        int nElements = 5;
+        for (int i = 1; i < nElements + 1; i++) {
+            stack.push("Element " + i);
+            System.out.println("Element " + i + " hinzugefügt.");
+        }
+        System.out.println("Elemente im Stack, nach Reihenfolge der Entnahme: -> " + stack.toString());
+        System.out.println();
+
+        for (int i = 1; i < nElements + 1; i++) {
+            System.out.println(stack.pop() + " entfernt.");
+            System.out.println("Neue Reihenfolge im Stack: -> " + stack.toString());
+            System.out.println();
+        }
+
+        System.out.println("Der Stack sollte leer sein: " + new Stack().toString().equals("[]"));
+
+        // null tests again for good measure
+        try {
+            stack.peek();
+        } catch (Exception e) {
+            System.out.println("obj.peek() exception caught: " + e);
+        }
+        try {
+            stack.pop();
+        } catch (Exception e) {
+            System.out.println("obj.pop() exception caught: " + e);
+        }
+    }
+}
